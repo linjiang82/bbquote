8415c1bdd8185f221ee67d01cdaff85183f0c7db
jiang.lin <jiang.lin@ladbrokes.com.au>
2023-11-11 22:12:44 +1000

    feat: project setup and tabview

--- BB Quote/ContentView.swift
+++ BB Quote/ContentView.swift
@@ -9,13 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            
+            Text("Breaking bad")
+                .tabItem { Label("Breaking Bad", systemImage: "tortoise") }
+            Text("Call soul")
+                .tabItem { Label("Better Call Saul", systemImage: "briefcase") }
         }
-        .padding()
+        .onAppear{
+             UITabBar.appearance().scrollEdgeAppearance = UITabBarAppearance()
+        }
+        
     }
 }
 
