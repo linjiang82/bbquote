e3a1e7a0f87e23bf7bef761fb8b9c8692fc28bf3
jiang.lin <jiang.lin@ladbrokes.com.au>
2023-11-11 21:21:41 +1000

    Initial Commit

---
+++ BB Quote/BB_QuoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  BB_QuoteApp.swift
+//  BB Quote
+//
+//  Created by Jiang Lin on 11/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct BB_QuoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
